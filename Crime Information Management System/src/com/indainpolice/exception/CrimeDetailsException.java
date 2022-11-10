fec8b7c3afc1b6e4d217f840f226e524ea155f29
rishabh2298 <rishabhmishra2298@gmail.com>
2022-11-11 00:16:55 +0530

    Exception handling files done

---
+++ Crime Information Management System/src/com/indainpolice/exception/CrimeDetailsException.java
@@ -0,0 +1,13 @@
+package com.indainpolice.exception;
+
+public class CrimeDetailsException extends Exception {
+	
+	public CrimeDetailsException() {
+		
+	}
+	
+	public CrimeDetailsException(String messsage) {
+		super(messsage);
+	}
+
+}
---
+++ Crime Information Management System/src/com/indainpolice/exception/DepartmentException.java
@@ -0,0 +1,13 @@
+package com.indainpolice.exception;
+
+public class DepartmentException extends Exception {
+	
+	public DepartmentException() {
+		
+	}
+	
+	public DepartmentException(String message) {
+		super(message);
+	}
+
+}
